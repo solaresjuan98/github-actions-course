5073b481088434e6cd8f0321386fb1a92d932f0d
Juan Antonio Solares <50550951+solaresjuan98@users.noreply.github.com>
2023-08-21 16:41:37 -0600

    fix hola_mundo.py

--- hola_mundo.py
+++ hola_mundo.py
@@ -4,7 +4,7 @@ import os
 #hello_world.py
 def hello_world():
 	nombre = os.getenv("USERNAME")
-	print("Hello {USERNAME} desde GitHub Actions!!")
+	print("Hello {nombre} desde GitHub Actions!!")
 
 if __name__ == '__main__':
 	hello_world()
